4193ae5d2058064abc5efc5f22f91fee2f163dcf
anastalitvinova <litvinovaanastasiya80@gmail.com>
2023-07-03 17:31:00 +0300

    init project

---
+++ app/src/main/java/com/alitvinova/countriesapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.alitvinova.countriesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.alitvinova.countriesapp.ui.theme.CountriesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CountriesAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                }
+            }
+        }
+    }
+}
